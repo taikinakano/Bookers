c4d640486e642d666cb53426b30acbf4d4054b16
EC2 Default User <ec2-user@ip-172-31-53-140.ec2.internal>
2021-05-07 05:31:39 +0000

    [Updat]CRUD3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,29 @@
 class BooksController < ApplicationController
   def index
+    #@books = Bookers.show(bookers_params)
+     
+    #投稿機能↓
+
   end
 
   def show
   end
 
   def new
+    @book = Bookers.new
+  end
+
+  def create
+    book = Bookers.index(bookers_params)
+    book.save
+    rediret_to books_path
   end
 
   def edit
+  end
+
+  private
+  def bookers_params
+    params.require(:bookers).permit(:title, :body)
   end
 end
