12c7fd8a5b357b209fd4a337f66f988a02b71715
taikinakano <kabayaki3323@gmail.com>
2021-05-08 14:53:03 +0000

    alert message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,9 +14,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+     redirect_to books_path(@book.id)
+    else
+     @books = Book.all
+     render "index"
+    end
   end
 
   def edit
@@ -28,7 +32,7 @@ class BooksController < ApplicationController
     book.update(book_params)
     redirect_to book_path(book)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
