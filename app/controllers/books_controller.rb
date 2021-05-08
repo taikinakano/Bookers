cc5366db1c75a5ac3f8821beacc68ceb40679e6a
taikinakano <kabayaki3323@gmail.com>
2021-05-08 07:39:56 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,7 +26,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to books_path(book)
+    redirect_to book_path(book)
   end
   
   def destroy
